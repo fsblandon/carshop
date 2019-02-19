6d886ce2f808e9765eb9b9809f4e9633e0093847
Santiago Blandon <fsblandon@MacBook-Pro-de-Santiago.local>
2019-02-19 08:24:38 -0500

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { CompareComponent } from './components/compare/compare.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    CompareComponent
   ],
   imports: [
     BrowserModule
